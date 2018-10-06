32ddc14cd9c65930d3b4ce9d94743de4ab5eefb5
dangrt5 <randy.dang05@gmail.com>
2018-10-05 18:19:36 -0700

    project set up with routing

---
+++ src/components/movie_quote.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default props => (
+  <div className="center">
+    <h1>Movie Quote</h1>
+    <h4>Run Forrest Run</h4>
+  </div>
+)
---
+++ src/components/sign_up.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="center">Sign Up</h1>
+      </div>
+    );
+  }
+};
+
+export default SignUp;
