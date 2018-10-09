a52f514d9a4c878be85350b617bef3c88fde7e14
dangrt5 <randy.dang05@gmail.com>
2018-10-09 13:03:17 -0700

    sign in added

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,19 @@ import axios from "axios";
 export const signUp = (userInfo) => {
   return async (dispatch) => {
     try {
-      const resp = await axios.post("http://api.reactprototypes.com/signup", userInfo)
+      const resp = await axios.post("http://api.reactprototypes.com/signup", userInfo);
       console.log("Sign Up Response", resp);
     } catch(error) {
         console.log("Sign Up Error:", error.message);
     }
+  }
+}
+
+export const signIn = userInfo => async dispatch => {
+  try {
+    const resp = await axios.post("http://api.reactprototypes.com/signin", userInfo);
+    console.log("Sign In Response:", resp);
+  } catch(error) {
+      console.log("Sign In Error:", error);
   }
 }
